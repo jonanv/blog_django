2abb8b19222635474454b4d052350d4807236251
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-09-13 19:33:37 -0500

    Se crea rutas para home y vista para home, se importa mainapp a settings

--- blog_django/urls.py
+++ blog_django/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# Imports
+from mainapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Routes
+    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Metodo de la vista home.html
+def home(request):
+    return render(request, 'mainapp/home.html', {
+        'title': 'Inicio'
+    })
