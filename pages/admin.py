b2ede8093ad8cd09e66c682181ea9908412fe487
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-09-14 21:07:22 -0500

    Se crean title y subtitle para el panel

--- pages/admin.py
+++ pages/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 # Imports
 from .models import Page
 
+# Configuracion de titulo del panel
+title = 'Proyecto con Django'
+subtitle = 'Panel de gestión'
+
+admin.site.site_title = title
+admin.site.site_header = title
+admin.site.index_title = subtitle
+
 # Register your models here.
 # Registrar modelo en el panel
 admin.site.register(Page)
