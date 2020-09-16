447a610b557d541879a9cbd346b2a96951afc527
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-09-15 21:52:12 -0500

    Se separan las urls por modulos

--- blog_django/urls.py
+++ blog_django/urls.py
@@ -14,17 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 # Imports
-from mainapp import views
-from pages import views as page_views
+# from mainapp import views
+# from pages import views as page_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Routes
-    path('', views.home, name='home'),
-    path('home/', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('page/<str:slug>', page_views.page, name='page'),
+    # path('', views.home, name='home'),
+    # path('home/', views.home, name='home'),
+    # path('about/', views.about, name='about'),
+    # path('page/<str:slug>', page_views.page, name='page'),
+    path('', include('mainapp.urls')),
+    path('', include('pages.urls')),
 ]
---
+++ pages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('page/<str:slug>', views.page, name='page'),
+]
+
+
