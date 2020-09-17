f96b37c530b10ecfec9ae30c85380d6b564194ec
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-09-16 22:32:14 -0500

    Se corrigue campo en el modelo de Article y se agrega updated_at y se hace migracion nuevamente

--- blog/admin.py
+++ blog/admin.py
@@ -5,10 +5,10 @@ from .models import Category, Article
 
 # Imports the names
 class CategoryAdmin(admin.ModelAdmin):
-    readonly_fields('created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
 
 class ArticleAdmin(admin.ModelAdmin):
-    readonly_fields('created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
 
 # Register your models here.
 admin.site.register(Category, CategoryAdmin)
