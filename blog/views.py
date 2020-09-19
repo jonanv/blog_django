938c7e3666a1baa3f2f1e338f6b3f5ae6d10c5f8
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-09-19 12:28:46 -0500

    Se agrega estructura de la template para la vista de la lista de articulos y se crea un for interno que recorre las categorias

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from blog.models import Article, Category
 
 # Create your views here.
 def articles(request):
-    
+    articles = Article.objects.all()
+
     return render(request, 'articles/list.html', {
-        'title': 'Artículos'
+        'title': 'Artículos',
+        'articles': articles
     })
