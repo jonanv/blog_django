175619d1fb76500c80cb52faf94eaf12dfcf726b
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-09-19 17:02:25 -0500

    Se crea pagina detail para la informacion de cada article y se crea ruta

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('articles/', views.list, name='list_articles'),
-    path('category/<int:category_id>', views.category, name='category')
+    path('category/<int:category_id>', views.category, name='category'),
+    path('article/<int:article_id>', views.article, name='article'),
 ]
