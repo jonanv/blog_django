03a701c6dc2768b284cc84ae71eac429c2ccf11e
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-09-19 16:24:35 -0500

    Se utiliza el metodo en las vistas de get_object_or_404

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Article, Category
 
 # Create your views here.
@@ -11,7 +11,7 @@ def list(request):
     })
 
 def category(request, category_id):
-    category = Category.objects.get(id=category_id)
+    category = get_object_or_404(Category, id=category_id)
 
     return render(request, 'categories/category.html', {
         'title': category.name,
