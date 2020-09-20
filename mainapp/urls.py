f368facd7e60d561638781b4c40c717a6e7838c0
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-09-19 21:03:23 -0500

    Se restringue el acceso con el decorador login_required()

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('register/', views.register_page, name='register'),
     path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -21,37 +21,46 @@ def about(request):
     })
 
 def register_page(request):
-    register_form = RegisterForm()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        register_form = RegisterForm()
 
-    if request.method == 'POST':
-        register_form = RegisterForm(request.POST)
+        if request.method == 'POST':
+            register_form = RegisterForm(request.POST)
 
-        if register_form.is_valid:
-            register_form.save()
-            messages.success(request, 'Te has registrado satisfactoriamente')
-            return redirect('home')
-        else:
-            register_form = RegisterForm()
+            if register_form.is_valid:
+                register_form.save()
+                messages.success(request, 'Te has registrado satisfactoriamente')
+                return redirect('home')
+            else:
+                register_form = RegisterForm()
 
-    return render(request, 'users/register.html', {
-        'title': 'Registro',
-        'register_form': register_form
-    })
+        return render(request, 'users/register.html', {
+            'title': 'Registro',
+            'register_form': register_form
+        })
 
 def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
 
-        user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.warning(request, 'No te has identificado correctamente')
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.warning(request, 'No te has identificado correctamente')
+        return render(request, 'users/login.html', {
+            'title': 'Identificación'
+        })
 
-    return render(request, 'users/login.html', {
-        'title': 'Identificación'
-    })
+def logout_user(request):
+    logout(request)
+    return redirect('login')
